7a7de3c97311815a56ced3e9d238a6f8ead0ea81
Lino Galiana <linogaliana@yahoo.fr>
2021-02-02 11:06:28 +0100

    html et pdf documents

--- R/build_website.R
+++ R/build_website.R
@@ -1,17 +1,54 @@
-build_website <- function(...){
+#' @export
+#' @importFrom bookdown render_book
+html_document <- function(output_dir = "_public",
+                          ...){
 
   # locations of resource files in the package
   pkg_resource <- function(...) {
     system.file(..., package = "utilitr")
   }
 
-  css <- list.files(
-    sprintf("%s/rmarkdown/templates/resources/css",
+ invisible(
+   check_structure()
+ )
+
+  css <- c(list.files(
+    sprintf("%s/rmarkdown/resources/css",
             pkg_resource()),
     full.names = TRUE
-  )
+  ), css)
+
+  bookdown::render_book(output_dir = output_dir,
+                        css = css,...)
 
 
+}
 
+
+check_structure <- function(){
+
+  pkg_resource <- function(...) {
+    system.file(..., package = "utilitr")
+  }
+
+  default_files <- list.files(sprintf("%s/rmarkdown/resources",
+                      pkg_resource()), recursive = TRUE)
+  default_files <- default_files[!startsWith(default_files, "css")]
+  default_dir <- list.dirs(sprintf("%s/rmarkdown/resources",
+                                   pkg_resource()), recursive = TRUE,
+                           full.names = FALSE)
+  default_dir <- default_dir[!grepl(default_dir, pattern = "(^|css)$")]
+
+  lapply(default_dir, function(fl){
+    if (isFALSE(dir.exists(fl))) dir.create(fl)
+  })
+
+  lapply(default_files, function(fl){
+
+    if (isFALSE(file.exists(fl))) file.copy(from = sprintf("%s/rmarkdown/resources/%s",
+                                                   pkg_resource(), fl),
+                                    to = fl)
+
+  })
 
 }
--- R/pdf_document.R
+++ R/pdf_document.R
@@ -1,3 +1,23 @@
-pdf_document <- function(...){
+
+#' @importFrom pagedown html_paged chrome_print
+#' @export
+
+pdf_document <- function(output_dir = "_public",
+                         extra_args = c('--disable-gpu', '--no-sandbox'),
+                         timeout = timeout,
+                         verbose = verbose,
+                         ...){
+
+  bookdown::render_book(input,
+                        output_format = 'pagedown::html_paged',
+                        output_file = '_pagedown_output/index.html')
+
+  pagedown::chrome_print('_pagedown_output/index.html',
+                         '_pagedown_output/DocumentationR.pdf',
+                         extra_args = extra_args,
+                         timeout = timeout,
+                         options = list(transferMode = 'ReturnAsStream'),
+                         verbose = verbose)
 
 }
+
