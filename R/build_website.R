192e874bec2b1259021d4388b67c7f887f4a78e8
Lino Galiana <linogaliana@yahoo.fr>
2021-02-09 11:39:02 +0100

    html_document model copied from gitbook model

--- R/build_website.R
+++ R/build_website.R
@@ -1,53 +1,54 @@
-#' @inheritParams bookdown::render_book
-#' @export
-#' @importFrom bookdown render_book
-html_document <- function(extra_dependencies = NULL, ...){
-
-  # locations of resource files in the package
-  pkg_resource <- function(...) {
-    system.file(..., package = "utilitr")
-  }
-
-  # invisible(
-  #   check_structure()
-  # )
-
-  # css_default <- c(
-  #   pkg_resource("/rmarkdown/resources/css/default.css"),
-  #   pkg_resource("/rmarkdown/resources/css/style.css")
-  # )
-
-  # if (is.null(css)){
-  #   css <- css_default
-  # } else{
-  #   css <- c(css_default, css)
-  # }
-
-  extra_dependencies <- c(
-    utilitr_dependencies(),
-    extra_dependencies
-  )
-
-  rmarkdown::html_document(
-    extra_dependencies = extra_dependencies,
-    # includes = includes,
-    ...
-  )
-
-
-}
+# #' @export
+# #' @importFrom bookdown render_book
+# html_document <- function(extra_dependencies = NULL, ...){
+#
+#   # locations of resource files in the package
+#   pkg_resource <- function(...) {
+#     system.file(..., package = "utilitr")
+#   }
+#
+#   # invisible(
+#   #   check_structure()
+#   # )
+#
+#   # css_default <- c(
+#   #   pkg_resource("/rmarkdown/resources/css/default.css"),
+#   #   pkg_resource("/rmarkdown/resources/css/style.css")
+#   # )
+#
+#   # if (is.null(css)){
+#   #   css <- css_default
+#   # } else{
+#   #   css <- c(css_default, css)
+#   # }
+#
+#   extra_dependencies <- c(
+#     utilitr_dependencies(),
+#     extra_dependencies
+#   )
+#
+#   rmarkdown::html_document(
+#     extra_dependencies = extra_dependencies,
+#     # includes = includes,
+#     ...
+#   )
+#
+#
+# }
 
 #' @importFrom htmltools htmlDependency
 
+utilitr_dependencies <- function(type = c("html","pdf")){
 
-utilitr_dependencies <- function() {
+  type <- match.arg(type)
+  files <- c("default.css", "style.css")
+  if (type == "pdf") files <- c(files, "default-fonts.css",
+                                "default-page.css")
+
   # default CSS stylesheet
   default_dep <- list(htmltools::htmlDependency(
     'utilitr-default', utils::packageVersion('utilitr'),
-    src = pkg_resource('css'), stylesheet = c(
-      file.path("default.css"),
-      file.path("style.css")
-    )
+    src = pkg_resource('rmarkdown/resources/css'), stylesheet = files
   ))
 
   return(default_dep)
@@ -59,52 +60,54 @@ pkg_resource <- function(...) {
 }
 
 
-#' #' @inheritParams bookdown::render_site
-#' #' @export
-#' # importFrom bookdown clean_book load_config render_book_script find_book_dir find_book_name
+
+#' The utilitR GitBook output format
 #'
-#' render_site <- function(input, ...)  {
-#'   on.exit(opts$restore(), add = TRUE)
-#'   oldwd = setwd(input)
-#'   on.exit(setwd(oldwd), add = TRUE)
-#'   config = load_config()
-#'   name = find_book_name(config, basename(normalizePath(input)))
-#'   book_dir = find_book_dir(config)
-#'   render = function(input_file, output_format, envir, quiet,
-#'                     encoding, ...) {
-#'     if (is.null(input_file)) {
-#'       in_dir(input, render_book_script(output_format,
-#'                                        envir, quiet))
-#'     }
-#'     else {
-#'       render_book(input_file, output_format, envir = envir,
-#'                   preview = TRUE)
-#'     }
-#'   }
-#'   clean = function() {
-#'     suppressMessages(bookdown::clean_book(clean = FALSE))
-#'   }
-#'   list(name = name, output_dir = book_dir, render = render,
-#'        clean = clean)
-#' }
+#' @param ... Additional arguments passed to \code{bookdown::\link{gitbook}()}.
+#' @return An R Markdown output format object to be passed to
+#'   \code{rmarkdown::\link{render}()}.
+#' @export
+gitbook_utilitr <- function(extra_dependencies = list(),
+                         ...) {
+  extra_dependencies <- c(
+    utilitr_dependencies(),
+    extra_dependencies
+  )
+
+  bookdown::gitbook(extra_dependencies = extra_dependencies, ...)
+
+}
+
+
+#' @export
+html_document <- gitbook_utilitr
+
+
+#' The utilitR PDF output format
 #'
-#'
-#'
-#' # render_book_script <- function(output_format = NULL, envir = globalenv(), quiet = TRUE){
-#' #   result = 0
-#' #   if (length(script <- existing_r("_render", TRUE))) {
-#' #     result = Rscript(c(if (quiet) "--quiet", script, shQuote(output_format)))
-#' #   }
-#' #   else if (file.exists("Makefile")) {
-#' #     result = system2("make")
-#' #   }
-#' #   else {
-#' #     render_book("index.Rmd", output_format = output_format,
-#' #                 envir = envir)
-#' #   }
-#' #   if (result != 0)
-#' #     stop("Error ", result, " attempting to render book")
-#' # }
+#' @param ... Additional arguments passed to \code{pagedown::\link{gitbook}()}.
+#' @return An R Markdown output format object to be passed to
+#'   \code{rmarkdown::\link{render}()}.
+#' @export
+pdf_utilitr <- function(extra_dependencies = list(),
+                            ...) {
+
+  extra_dependencies <- c(
+    utilitr_dependencies(type = "pdf"),
+    extra_dependencies
+  )
+
+  bookdown::render_book('index.Rmd',
+                        ..., output_format = 'pagedown::html_paged',
+                        output_file = '_pagedown_output/index.html',
+                        extra_dependencies = extra_dependencies)
+
+  pagedown::chrome_print('_pagedown_output/index.html', '_pagedown_output/DocumentationR.pdf',
+                         extra_args = c('--disable-gpu', '--no-sandbox'),
+                         timeout = 600,
+                         options = list(transferMode = 'ReturnAsStream'),
+                         verbose = 1)
+}
 
 
 
--- R/pdf_document.R
+++ R/pdf_document.R
@@ -2,22 +2,39 @@
 #' @importFrom pagedown html_paged chrome_print
 #' @export
 
-pdf_document <- function(output_dir = "_public",
+pdf_document <- function(..., css = NULL,
                          extra_args = c('--disable-gpu', '--no-sandbox'),
                          timeout = timeout,
-                         verbose = verbose,
-                         ...){
+                         verbose = verbose){
 
-  bookdown::render_book(input,
-                        output_format = 'pagedown::html_paged',
-                        output_file = '_pagedown_output/index.html')
+  # css_default <- c(
+  #   pkg_resource("/rmarkdown/resources/css/default.css"),
+  #   pkg_resource("/rmarkdown/resources/css/style.css"),
+  #   pkg_resource("/rmarkdown/resources/css/default-fonts.css"),
+  #   pkg_resource("/rmarkdown/resources/css/default-page.css")
+  # )
+  #
+  # css <- c(css_default, css)
+  #
+  # bookdown::render_book("index.Rmd", ..., css = css,
+  #                       output_format = 'pagedown::html_paged',
+  #                       output_file = '_pagedown_output/index.html')
 
-  pagedown::chrome_print('_pagedown_output/index.html',
-                         '_pagedown_output/DocumentationR.pdf',
-                         extra_args = extra_args,
-                         timeout = timeout,
+  extra_dependencies <- c(
+    utilitr_dependencies(type = "pdf"),
+    extra_dependencies
+  )
+
+  bookdown::render_book('index.Rmd',
+                        ..., output_format = 'pagedown::html_paged',
+                        output_file = '_pagedown_output/index.html',
+                        extra_dependencies = extra_dependencies)
+
+  pagedown::chrome_print('_pagedown_output/index.html', '_pagedown_output/DocumentationR.pdf',
+                         extra_args = c('--disable-gpu', '--no-sandbox'),
+                         timeout = 600,
                          options = list(transferMode = 'ReturnAsStream'),
-                         verbose = verbose)
+                         verbose = 1)
 
 }
 
