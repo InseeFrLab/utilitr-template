376a895f2cdfbb44c6faa0c49ed4b2b7baa6cf2d
Lino Galiana <linogaliana@yahoo.fr>
2021-02-09 15:25:40 +0100

    ajout test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(utilitr)
+
+test_check("utilitr")
---
+++ tests/testthat/test-markdown.R
@@ -0,0 +1,22 @@
+# inspired by mitchell O'Hara-Wild vitae package : https://github.com/mitchelloharawild/vitae/blob/master/tests/testthat/test-template.R
+# borrowed from https://github.com/spyrales/gouvdown/blob/master/tests/testthat/test-rmarkdown.R
+
+expect_render <- function(template){
+
+  outputdir <- tempfile()
+  dir.create(outputdir)
+  dir.create(file.path(outputdir,'www'))
+  file.copy(system.file("rmarkdown", "templates", template, "skeleton", "skeleton.Rmd", package = "utilitr"),outputdir)
+  file <- file.path(outputdir,"skeleton.Rmd")
+  testthat::expect_output(
+    testthat::expect_message(
+      rmarkdown::render(
+        file,
+        output_dir = outputdir),
+      "Output created"),
+    "pandoc")
+}
+
+testthat::test_that("utilitr_html_document", {
+  expect_render("html_document")
+})
