26a268296ba59a347f3738286e1f1b3fabf097b3
Meslin Olivier <olivier.meslin@insee.fr>
2021-02-22 11:17:11 +0100

    Moi parler anglais

--- R/utilitr.R
+++ R/utilitr.R
@@ -1,6 +1,6 @@
-#' utilitr: A package to locally build our reproducible documentation
+#' utilitr: A package to build our documentation locally
 #'
-#' This package is useful if you want to build locally our documentation.
+#' This package is useful if you want to build our documentation locally.
 #'  If you expect a series of functions from the documentation, this
 #'  is not the right place
 #'
