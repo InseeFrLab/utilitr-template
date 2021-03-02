9898a3030fc68ac89bb36be75763f607d2c6ac1b
Lino Galiana <linogaliana@yahoo.fr>
2021-03-02 09:58:17 +0100

    chemins plus clean

--- R/build_website.R
+++ R/build_website.R
@@ -8,16 +8,14 @@
 utilitr_dependencies <- function(type = c("html","pdf"), to_list = FALSE){
 
   type <- match.arg(type)
+
+  files <- c("default-fonts.css",
+    "default.css",
+    "style-utilitr.css",
+    "icones-fa.css")
+
   if (type == "pdf") {
-    files <-
-      c("default-fonts.css",
-        "default.css",
-        "style-utilitr.css",
-        "icones-fa.css",
-        "default-page.css")
-  }
-  if (type == "html") {
-    files <- NULL
+    files <- c(files, "default-page.css")
   }
 
   if (to_list) return(paste0(pkg_resource('rmarkdown/resources/css/'), files))
@@ -54,10 +52,7 @@ gitbook_utilitr <- function(extra_dependencies = list(),
 
   bookdown::gitbook(extra_dependencies = extra_dependencies,
                     new_session = TRUE,
-                    css = c("inst/rmarkdown/resources/css/default-fonts.css",
-                            "inst/rmarkdown/resources/css/default.css",
-                            "inst/rmarkdown/resources/css/style-utilitr.css",
-                            "inst/rmarkdown/resources/css/icones-fa.css"),
+                    css = utilitr_dependencies(type = "html", to_list = TRUE),
                     ...)
 
 }
