81864ebbcaa5fc40348255c3fe698b91bfb806fa
Lino Galiana <linogaliana@yahoo.fr>
2021-03-08 18:34:08 +0100

    clean bar

--- R/test_template.R
+++ R/test_template.R
@@ -2,6 +2,6 @@ test_template <- function(){
   unlink(c('_public'), recursive = TRUE)
   rmarkdown::draft('index.Rmd', template='utilitr', package='utilitr', edit = FALSE)
   bookdown::render_book('index.Rmd', output_dir = '_public', output_format = 'utilitr::html_document')
-  unlink(c("index.Rmd","_output.yml",'_bookdown.yml'), recursive = TRUE)
+  unlink(c("index.Rmd","_output.yml",'_bookdown.yml','style.css'), recursive = TRUE)
   unlink(c('chapter1','images'), recursive = TRUE)
 }
