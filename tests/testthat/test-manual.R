f766c112875a1418a6171ecd057f458fc13af877
Lino Galiana <33896139+linogaliana@users.noreply.github.com>
2021-03-16 11:44:07 +0100

    Tests plus propres (#11)
    close #10
    
    * test html in tempdir
    
    * ajout dep rstudioapi
    
    * retire fonction test_template

---
+++ tests/testthat/test-manual.R
@@ -0,0 +1,38 @@
+testthat::context("Manual check for the documentation")
+
+
+destdir <- tempdir()
+
+unlink(sprintf("%s/test-utilitr", destdir))
+dir.create(sprintf("%s/test-utilitr", destdir))
+
+setwd(sprintf("%s/test-utilitr", destdir))
+
+
+rmarkdown::draft("index.Rmd", template='utilitr', package='utilitr', edit = FALSE)
+
+
+# TEST HTML --------------------
+
+testthat::test_that(
+  "HTML version is fine", {
+    testthat::skip_on_ci()
+    bookdown::render_book(sprintf("%s/test-utilitr/index.Rmd", destdir),
+                          output_dir = sprintf("%s/test-utilitr/_public", destdir),
+                          output_format = 'utilitr::html_document')
+    rstudioapi::viewer("_public/index.html")
+  }
+)
+
+
+# TEST PDF ----------------------
+
+testthat::test_that(
+  "PDF version is fine", {
+    testthat::skip_on_ci()
+    utilitr::pdf_document()
+    rstudioapi::viewer("_pagedown_output/DocumentationR.pdf")
+  }
+)
+
+
