f9245f56639302b04ba8d0f2ca09752cb8c304f9
Olivier Meslin <olivier.meslin@insee.fr>
2021-03-24 18:18:15 +0100

    Ajouter un JS pour construire le message date-fiche

---
+++ inst/rmarkdown/resources/js/date-fiche.js
@@ -0,0 +1,12 @@
+let message = new String();
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
+let yyyy = today.getFullYear();
+
+today = `${dd}/${mm}/${yyyy}`;
+
+message = 'Fiche téléchargée le ' + today;
+
+
+document.documentElement.style.setProperty('--date-fiche', `'${message}'`);
