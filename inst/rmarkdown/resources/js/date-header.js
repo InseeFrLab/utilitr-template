8c1319aba4f7d64039d4c4f4c984ccc23907ea0c
Olivier Meslin <olivier.meslin@insee.fr>
2021-03-31 16:06:29 +0200

    Indentation

--- inst/rmarkdown/resources/js/date-header.js
+++ inst/rmarkdown/resources/js/date-header.js
@@ -1,10 +1,10 @@
 {
-	let today = new Date();
-	let dd = String(today.getDate()).padStart(2, '0');
-	let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
-	let yyyy = today.getFullYear();
-	let datetoday = `${dd}/${mm}/${yyyy}`;
-	let message = 'Source: www.utilitr.org, date de téléchargement: ' + datetoday;
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
+  let yyyy = today.getFullYear();
+  let datetoday = `${dd}/${mm}/${yyyy}`;
+  let message = 'Source: www.utilitr.org, date de téléchargement: ' + datetoday;
 
-	document.documentElement.style.setProperty('--date-header', `'${message}'`);
+  document.documentElement.style.setProperty('--date-header', `'${message}'`);
 }
