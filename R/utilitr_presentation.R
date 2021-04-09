fe327bc677f7df76f66c8cfff8effd06e57de81c
Lino Galiana <linogaliana@yahoo.fr>
2021-04-09 20:19:57 +0200

    css pour prez

---
+++ R/utilitr_presentation.R
@@ -0,0 +1,28 @@
+utilitr_dependencies <- function(
+){
+
+  files <- c("default.css", "default-fonts",
+             "metropolis.css",
+             "custom.css")
+
+
+  default_dep <- list(htmltools::htmlDependency(
+    'utilitr-default', utils::packageVersion('utilitr'),
+    src = pkg_resource('rmarkdown/resources'),
+    stylesheet = paste0("css/", files)#,
+#    script = paste0("js/", script)
+  ))
+
+  return(default_dep)
+}
+
+#' @export
+utilitr_presentation <- function(extra_dependencies = list(),
+                          ...) {
+  extra_dependencies <- c(
+    utilitr_dependencies_presentation(),
+    extra_dependencies
+  )
+
+
+}
